a33a10712657705dbcc7689acc237294e9433af6
Khorally-s <kvpierre@gmail.com>
2023-05-05 17:16:16 -0400

    styling & state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-    return <div> App</div>
+    // this piece of state is getting defined here bc all children elements need access to it
+    const [books, setBooks] = useState([])
+    // the goal of the following functions are to modify the books piece of state in some way, then pass them down to other component files
+    //this is an eventHandler function
+    const createBook = (title) => {
+        console.log(title, 'is the title of book that needs to be added')
+    }
+    return <div> <BookCreate onCreate={createBook}/> </div>
 }
 
 export default App;
